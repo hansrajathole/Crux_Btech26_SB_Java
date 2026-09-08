e84afc03c16cba718c2342d9bb92b0dd0f084c0f
hansrajathole <hansrajathole1@gmail.com>
2026-09-08 12:48:20 +0530

    basics and datatypes

---
+++ Lec3/Demo.java
@@ -0,0 +1,14 @@
+package Lec3;
+
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("hello Duniya...");
+
+
+        int x = 20;
+        long l =1200808000000000000L;
+        System.out.println(x);
+
+    } 
+}
+
