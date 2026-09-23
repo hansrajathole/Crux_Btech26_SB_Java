36eb6cb34056d0d6f298a2d4a6f96c8ebbe99cf0
hansrajathole <hansrajathole1@gmail.com>
2026-09-23 21:35:49 +0530

    basic questions on Operator

---
+++ Lec8/DollerToRupees.java
@@ -0,0 +1,10 @@
+public class DollerToRupees {
+    public static void main(String[] args) {
+        int dollar = 8;
+        float fixVal = 95.53f;
+
+        float rupees = dollar*fixVal;
+
+        System.out.println("in indian rupees --->"+rupees);
+    }
+}
---
+++ Lec8/SimpleInterest.java
@@ -0,0 +1,14 @@
+
+
+public  class SimpleInterest{
+    
+    public  static  void main(String [] args){
+        int x = 10;
+
+        if(x%2 == 0){
+            System.out.println("The Given number is Even");
+        }else {
+            System.out.println("The Given number is Odd");
+        }
+    }
+}
